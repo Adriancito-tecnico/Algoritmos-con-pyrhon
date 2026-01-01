725ed1b2a4bc12959e60c9200fdad72e70166dbb
Adriancito-tecnico <adrian1101.0110.1100@gmail.com>
2026-01-02 00:42:51 +0100

    Delete Contador de palabras 1.0.0.py

--- Contador de palabras 1.0.0.py
+++
@@ -1,6 +0,0 @@
-texto = "Este es un ejemplo de texto para contar el número de palabras."
-
-palabras = texto.split() 
-total_palabras = len(palabras)
-
-print(f"El texto contiene {total_palabras} palabras.")
