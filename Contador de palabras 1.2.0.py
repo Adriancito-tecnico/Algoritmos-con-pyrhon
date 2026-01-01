5480c2a82288197810d9a696a3e28d56a39d9cb8
Adriancito-tecnico <adrian1101.0110.1100@gmail.com>
2026-01-02 00:40:31 +0100

    Add files via upload

---
+++ Contador de palabras 1.2.0.py
@@ -0,0 +1,6 @@
+texto = "Este es un ejemplo de texto para contar el número de palabras."
+
+palabras = texto.split() 
+total_palabras = len(palabras)
+
+print(f"El texto contiene {total_palabras} palabras.")
